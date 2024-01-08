1aed56ea21132fd53ced97e4d842e5388524cdee
SergeyKove <153372120+SergeyKove@users.noreply.github.com>
2024-01-08 22:20:39 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -12,40 +12,49 @@ const input = document.getElementById('adText')
 
 for(let i = 0; i < todos.length; ++i) {
     let names = todos[i].name;
-    addListItem(names)
+    let number = todos[i].id;
+    addListItem(names, number)
 }
 
-function addListItem(value) {
+function addListItem(value, value2) {
     let li = document.createElement('li');
     li.innerText = value;
+
     let doneButton = document.createElement('button')
     doneButton.classList.add("doneButton")
+    doneButton.addEventListener('click', () => onDone(value2))
+
     let editButton = document.createElement('button')
     editButton.classList.add("editButton")
+
     let deleteButton = document.createElement('button')
     deleteButton.classList.add("deleteButton")
+    deleteButton.addEventListener('click', () => onDelete(value2))
+
+    li.setAttribute('id', value2)
     list.appendChild(li);
     li.appendChild(doneButton);
     li.appendChild(editButton);
     li.appendChild(deleteButton);
 }
 
-function deleteButtonId(value) {
-    document.getElementsByClassName("deleteButton")[value].id = "deleteButton" + value;
+function onDelete(value2) {
+    let el = document.getElementById(value2);
+    el.remove();
 }
 
-for (let i = 0; i < 4; ++i) {
-    deleteButtonId(i)
+function onDone(value2) {
+    let el = document.getElementById(value2);
+    el.style.textDecoration = 'line-through';
 }
 
-function deleteTodo() {
-    const index = todos.filter(item => item.id !== '1');
-    todos.splice(index, 1);
-    console.log(todos)
-}
-
-const delButton = document.getElementById('deleteButton0')
-delButton.addEventListener('click', deleteTodo)
+// function deleteTodo(value) {
+//     const index = todos.filter(item => item.id !== '1');
+//     todos.splice(index, 1);
+//     console.log(todos);
+// }
+// const delButton = document.getElementById('1')
+// delButton.addEventListener('click', deleteTodo())
 
 button.addEventListener('click', createTodo)
 function createTodo() {
